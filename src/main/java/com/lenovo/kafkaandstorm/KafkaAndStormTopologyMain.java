d28e5046003dbf0654ac1389bb71320a5704fe7c
liulei18 <liulei18@lenovo.com>
2018-06-05 16:35:03 +0800

    update

---
+++ src/main/java/com/lenovo/kafkaandstorm/KafkaAndStormTopologyMain.java
@@ -0,0 +1,27 @@
+package com.lenovo.kafkaandstorm;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.ZkHosts;
+
+public class KafkaAndStormTopologyMain {
+    public static void main(String[] args) throws Exception{
+        TopologyBuilder topologyBuilder = new TopologyBuilder();
+        topologyBuilder.setSpout("kafkaSpout",new KafkaSpout(new SpoutConfig(new ZkHosts("mini2:2181,mini3:2181,mini4:2181"),"orderMq","/myKafka","kafkaSpout")),1);
+        topologyBuilder.setBolt("mybolt1",new ParserOrderMqBolt(),1).shuffleGrouping("kafkaSpout");
+
+        Config config = new Config();
+        config.setNumWorkers(1);
+        //提交任务 -----两种模式 本地模式和集群模式
+        if (args.length>0) {
+            StormSubmitter.submitTopology(args[0],config,topologyBuilder.createTopology());
+        } else {
+            LocalCluster localCluster = new LocalCluster();
+            localCluster.submitTopology("storm2kafka",config,topologyBuilder.createTopology());
+        }
+    }
+}
---
+++ src/main/java/com/lenovo/order/OrderMqSender.java
@@ -0,0 +1,28 @@
+package com.lenovo.order;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+import java.util.Properties;
+
+public class OrderMqSender {
+    public static void main(String[] args) {
+        String TOPIC = "orderMq";
+        Properties props = new Properties();
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("metadata.broker.list", "mini2:9092,mini3:9092,mini4:9092");
+        props.put("request.required.acks", "1");
+        props.put("partitioner.class", "kafka.producer.DefaultPartitioner");
+        Producer<String, String> producer = new Producer<>(new ProducerConfig(props));
+        for (int messageNo = 1; messageNo < 10000; messageNo++) {
+            producer.send(new KeyedMessage<String, String>(TOPIC, messageNo + "",new OrderInfo().random() ));
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
